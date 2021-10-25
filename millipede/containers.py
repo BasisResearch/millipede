4324a7387d951b8d2b30f3240ffdc96cc7dba12a
martin jankowiak <jankowiak@gmail.com>
2021-10-25 19:56:05 -0400

    clean up containers.py

--- millipede/containers.py
+++ millipede/containers.py
@@ -7,15 +7,15 @@ from .util import stack_namespaces
 
 class SimpleSampleContainer(object):
     def __init__(self):
-        self._raw_samples = []
+        self._samples = []
 
     def __call__(self, sample):
-        self._raw_samples.append(sample)
+        self._samples.append(sample)
 
     @cached_property
     def samples(self):
-        samples = stack_namespaces(self._raw_samples)
-        del self._raw_samples
+        samples = stack_namespaces(self._samples)
+        del self._samples
         return samples
 
     @cached_property
@@ -38,26 +38,21 @@ class SimpleSampleContainer(object):
 
 class StreamingSampleContainer(object):
     def __init__(self):
-        self._raw_pip = None
         self._num_samples = 0.0
         self._weights = []
-        self._raw_beta = None
-        self._raw_gamma = None
 
     def __call__(self, sample):
         self._weights.append(sample.weight)
         self._num_samples += 1.0
-        if self._raw_pip is None:
+        if self._num_samples == 1.0:
             self._raw_pip = sample.add_prob * sample.weight
             self._raw_beta = sample.beta * sample.weight
             self._raw_gamma = sample.gamma * sample.weight
         else:
-            self._raw_pip = (1.0 - 1.0 / self._num_samples) * self._raw_pip +\
-                (sample.beta * sample.weight) / self._num_samples
-            self._raw_beta = (1.0 - 1.0 / self._num_samples) * self._raw_beta +\
-                (sample.beta * sample.weight) / self._num_samples
-            self._raw_gamma = (1.0 - 1.0 / self._num_samples) * self._raw_gamma +\
-                (sample.gamma * sample.weight) / self._num_samples
+            factor = 1.0 - 1.0 / self._num_samples
+            self._raw_pip = factor * self._raw_pip + (sample.beta * sample.weight) / self._num_samples
+            self._raw_beta = factor * self._raw_beta + (sample.beta * sample.weight) / self._num_samples
+            self._raw_gamma = factor * self._raw_gamma + (sample.gamma * sample.weight) / self._num_samples
 
     @cached_property
     def _normalizer(self):
