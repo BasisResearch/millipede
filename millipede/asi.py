ce56f14e2828d315c7fd77dfb9d8b58e85ab2b7d
martin jankowiak <jankowiak@gmail.com>
2021-11-26 10:05:35 -0800

    basic asi

--- millipede/asi.py
+++ millipede/asi.py
@@ -1,22 +1,14 @@
 from types import SimpleNamespace
 import math
-import time
-import argparse
-
-import numpy as np
 
 import torch
-from torch.distributions import Binomial, Bernoulli
+from torch.distributions import Bernoulli, Uniform
 from torch.linalg import norm
-from torch import randn, sigmoid
+from torch import sigmoid
 from torch import triangular_solve as trisolve
-from torch import cholesky_solve as chosolve
-
-from torch import einsum, matmul
-from torch.distributions import Categorical
 
 from .normal import NormalLikelihoodSampler
-from .util import get_loo_inverses, leave_one_out, safe_cholesky
+from .util import safe_cholesky
 
 
 class ASISampler(NormalLikelihoodSampler):
@@ -42,7 +34,10 @@ class ASISampler(NormalLikelihoodSampler):
 
         self.update_zeta_AD(0.0)
 
-        print("Initialized ASISampler with (N, P, S) = ({}, {}, {:.1f})".format(self.N, self.P, S))
+        self.uniform_dist = Uniform(0.0, torch.ones(1, device=X.device, dtype=X.dtype))
+
+        if verbose_constructor:
+            print("Initialized ASISampler with (N, P, S) = ({}, {}, {:.1f})".format(self.N, self.P, S))
 
     def logit_eps(self, x):
         x = (x - self.epsilon_asi) / (1.0 - x - self.epsilon_asi)
@@ -58,19 +53,19 @@ class ASISampler(NormalLikelihoodSampler):
         self.D = self.zeta * torch.min(torch.ones(1, device=pi.device, dtype=pi.dtype), (1.0 - pi) / pi)
 
     def initialize_sample(self, seed=None):
+        self.num_accepted = 0
         if seed is not None:
             torch.manual_seed(seed)
 
         sample = SimpleNamespace(gamma=torch.zeros(self.P, device=self.device).bool(),
-                                 add_prob=torch.zeros(self.P, device=self.device, dtype=self.dtype),
-                                 _active=torch.tensor([], device=self.device, dtype=torch.int64))
+                                 _active=torch.tensor([], device=self.device, dtype=torch.int64),
+                                 add_prob=torch.zeros(self.P, device=self.device, dtype=self.dtype))
+
         if self.compute_betas:
             sample.beta = torch.zeros(self.P, device=self.device, dtype=self.dtype)
 
         if self.include_intercept:
             sample._activeb = torch.tensor([self.P], device=self.device, dtype=torch.int64)
-
-        sample = self._compute_probs(sample)
 
         return sample
 
@@ -90,60 +85,62 @@ class ASISampler(NormalLikelihoodSampler):
         logq_prop = q_curr.log_prob(flips).sum().item()
         logq_curr = q_prop.log_prob(flips).sum().item()
 
-        _, activeb = self.get_active_indices(sample)
-        activeb_prop = torch.cat([torch.nonzero(gamma_prop).squeeze(-1), torch.tensor([self.P])])
-
-        log_target_curr, L_curr = self.compute_log_target(sample._omega, activeb=activeb)
-        log_target_prop, L_prop, beta_prop, beta_prop_active, beta_mean_prop = \
-            self.compute_log_target(sample._omega, activeb=activeb_prop, sample_beta=True)
+        log_target_curr, _, _ = self.compute_log_target(sample=sample)
+        log_target_prop, active_prop, activeb_prop = self.compute_log_target(gamma=gamma_prop)
 
         accept = log_target_prop - log_target_curr + logq_curr - logq_prop
         accept = min(1.0, accept.exp().item())
 
-        if self.t > self.Tb:
-            self.gamma_acc_probs.append(accept)
         accept_bool = self.uniform_dist.sample().item() < accept
 
         if accept_bool:
+            self.num_accepted += 1
             sample.gamma = gamma_prop
-            sample._beta = beta_prop
-            sample.beta_mean = beta_mean_prop
-            sample._psi = torch.mv(self.Xb[:, activeb_prop], beta_prop_active)
-            self.L_active = L_prop
-        else:
-            self.L_active = L_curr
+            sample._active = active_prop
+            if self.include_intercept:
+                sample._activeb = activeb_prop
+            # sample._beta = beta_prop
+            # sample.beta_mean = beta_mean_prop
 
-        sample.add_prob = self._compute_add_prob(sample)
+        sample.add_prob = sigmoid(self._compute_add_prob(sample))
 
-        if self.t <= self.Tb:
+        if self.t <= self.T_burnin:
             t = self.t
             self.pi = (t / (t + 1)) * self.pi + sample.add_prob / (t + 1)
             phi_t = 1.0 / math.pow(t, self.lambda_exponent)
             self.update_zeta_AD(phi_t * (accept - self.acc_target))
 
-        sample = self.sample_omega(sample)
         return sample
 
-    def compute_log_target(self, omega, sample=None, activeb=None, sample_beta=False):
-        if activeb is None:
-            _, activeb = self.get_active_indices(sample)
-        Xb_active = self.Xb[:, activeb]
-        num_active = activeb.size(-1) - 1
+    def compute_log_target(self, sample=None, gamma=None):
+        assert self.prior == "isotropic"
+
+        gamma = gamma if gamma is not None else sample.gamma
+
+        active = torch.nonzero(gamma).squeeze(-1)
+        activeb = active
+        if self.include_intercept:
+            activeb = torch.cat([active, torch.tensor([self.P])])
+
+        Xb_active = self.X[:, activeb]
+        num_active = activeb.size(-1)
         num_inactive = self.P - num_active
 
-        precision = Xb_active.t() @ (omega.unsqueeze(-1) * Xb_active)
+        precision = Xb_active.t() @ Xb_active
         precision.diagonal(dim1=-2, dim2=-1).add_(self.tau)
+        if self.include_intercept:
+            precision[-1, -1].add_(self.tau_intercept - self.tau)
 
-        L = torch.cholesky(precision, upper=False)
+        L = safe_cholesky(precision)
         LZ = trisolve(self.Z[activeb].unsqueeze(-1), L, upper=False)[0].squeeze(-1)
-        logdet = L.diag().log().sum() - L.size(-1) * self.half_log_tau
-        h_factor = num_active * self.log_h + num_inactive * self.log_1mh
+        logdet = -L.diag().log().sum()
+        h_factor = num_active * (self.log_h + 0.5 * math.log(self.tau)) + num_inactive * self.log_1mh
+        log_factor = logdet - 0.5 * self.N * (self.YY - norm(LZ).pow(2.0)).log() + h_factor
 
-        if sample_beta:
-            mean = chosolve(self.X_kappa[activeb].unsqueeze(-1), L).squeeze(-1)
-            beta_active = mean + trisolve(torch.randn(activeb.size(-1), 1), L, upper=False)[0].squeeze(-1)
-            beta, beta_mean = torch.zeros(self.P + 1), torch.zeros(self.P + 1)
-            beta[activeb], beta_mean[activeb] = beta_active, mean
-            return 0.5 * norm(LZ, dim=0).pow(2.0) - logdet + h_factor, L, beta, beta_active, beta_mean
+        # mean = chosolve(self.X_kappa[activeb].unsqueeze(-1), L).squeeze(-1)
+        # beta_active = mean + trisolve(torch.randn(activeb.size(-1), 1), L, upper=False)[0].squeeze(-1)
+        # beta, beta_mean = torch.zeros(self.P + 1), torch.zeros(self.P + 1)
+        # beta[activeb], beta_mean[activeb] = beta_active, mean
+        # return 0.5 * norm(LZ, dim=0).pow(2.0) - logdet + h_factor, L, beta, beta_active, beta_mean
 
-        return 0.5 * norm(LZ, dim=0).pow(2.0) - logdet + h_factor, L
+        return log_factor, active, activeb
